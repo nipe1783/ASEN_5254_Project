1c0d09c18f004865ad06450615d6bd2a67959d36
Nic Perrault <nipe1783@colorado.edu>
2023-11-30 09:55:09 -0700

    working ompl example in amp

--- src/ompl-test.cpp
+++ src/ompl-test.cpp
@@ -1,4 +1,16 @@
-#include "AMPCore.h"
+
+#include <ompl/geometric/SimpleSetup.h>
+#include <ompl/base/spaces/SE2StateSpace.h>
+#include <ompl/geometric/planners/rrt/RRT.h>
+#include <ompl/geometric/planners/sst/SST.h>
+#include "World.h"
+#include "StateValidityCheckerDatabase.h"
+#include "PostProcessing.h"
+
+
+namespace fs = std::filesystem;
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
 
 int main(int argc, char** argv) {
     std::cout << "Hello, world!" << std::endl;
